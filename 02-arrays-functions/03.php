618d1f7ea68ea189e4605460e8e962f8a30ed7c2
Matt Maslen <mattmaslen123@gmail.com>
2020-11-02 16:13:42 +0000

    q3 working version

---
+++ 02-arrays-functions/03.php
@@ -0,0 +1,22 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+/* 
+Update sameName to return true if the associative arrays have the same name property and false if they have different names.
+ */
+
+function sameName ($person1, $person2) {
+    // … your code here
+    return $person1["name"] === $person2["name"] ? true : false;
+};
+
+$a = [ "name" => "Alice", "age" => 52 ];
+$b = [ "name" => "Bob", "age" => 34 ];
+$c = [ "name" => "Charlie", "age" => 24 ];
+$d = [ "name" => "Bob", "age" => 97 ];
+
+dump(sameName($a, $b)); // false
+dump(sameName($b, $c)); // false
+dump(sameName($c, $d)); // false
+dump(sameName($b, $d)); // true

9ff834b78b825780f79a591afdab6bf602eabcb9
Matt Maslen <mattmaslen123@gmail.com>
2020-11-02 16:27:51 +0000

    q5 working version

---
+++ 02-arrays-functions/05.php
@@ -0,0 +1,18 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+/* 
+Create a function, divide, that takes two numbers as arguments. The function should return the first number divided by the second number.
+ */
+
+// … your function here
+
+function divide ($a, $b) {
+    return $a / $b;
+}
+
+dump(divide(4, 2)); // 2
+dump(divide(20, 2)); // 10
+dump(divide(5, 2)); // 2.5
+dump(divide(2.5, 0.5)); // 5

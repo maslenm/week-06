b503ddbd22af217362f9e92b0c7a0d30e6ada592
Matt Maslen <mattmaslen123@gmail.com>
2020-11-02 13:09:45 +0000

    q6 working version

---
+++ 01-basic-php/06.php
@@ -0,0 +1,30 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+/* 
+Write some code that outputs a cumulative total of the current line number for 1 to 50.
+
+e.g.
+
+1
+1 + 2
+1 + 2 + 3
+1 + 2 + 3 + 4
+etc.
+Example output:
+
+1
+3
+6
+10
+...
+1275
+ */
+
+
+
+for ($i = 1; $i <= 50; $i += 1) {
+    $total += $i;
+    dump($total);
+}

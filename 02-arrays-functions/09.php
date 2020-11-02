01851b160bfec19b73597c51eda9fa558d00e256
Matt Maslen <mattmaslen123@gmail.com>
2020-11-02 21:34:10 +0000

    q9 working version

---
+++ 02-arrays-functions/09.php
@@ -0,0 +1,33 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+/* 
+Create a function, squares, that takes an array of numbers as an argument. Return a new array containing the square of each number.
+
+Try it using foreach
+Try it using collect() (you'll need the illuminate/support package)
+ */
+
+// … your function here
+
+
+function squares ($values) {
+    
+    foreach ($values as $value) { 
+
+        $squares[] = $value * $value;
+        
+    };
+
+    return $squares;
+};
+
+
+
+
+
+dump(
+    squares([2, 3, 4]), // [4, 9, 16]
+    squares([2, 3, 4, 5, 6]), // [4, 9, 16, 25, 36]
+);

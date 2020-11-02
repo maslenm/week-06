3d18e80ff843ed06507cd530425625387a6701c1
Matt Maslen <mattmaslen123@gmail.com>
2020-11-02 16:38:22 +0000

    updated to clean code

--- 02-arrays-functions/07.php
+++ 02-arrays-functions/07.php
@@ -10,6 +10,6 @@ Create a function, average5, that takes five numbers as arguments. The function 
 
 function average5 ($a, $b, $c, $d, $e) {
     return (($a + $b + $c + $d + $e) / 5 );
-}
+};
 
 dump(average5(1, 2, 3, 4, 5)); // 3

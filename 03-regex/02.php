63a4f6d34e771e64ba1fa2d03dce0cfd620ed662
Matt Maslen <mattmaslen123@gmail.com>
2020-11-03 14:08:00 +0000

    q2 working version

---
+++ 03-regex/02.php
@@ -0,0 +1,18 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+/* 
+Create a function, mi5, that returns true if the given string is 00 followed by a digit. Make sure it returns a boolean value:
+ */
+
+// … your function here
+
+function mi5 ($str) {
+    return preg_match("/0{2}\d/", $str) === 1;
+};
+
+dump(mi5("007")); // true
+dump(mi5("oo8")); // false
+dump(mi5("003")); // true
+dump(mi5("M")); // false

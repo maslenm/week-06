aa3f2269f9a618dcdfb57a0c743466e794a7574d
Matt Maslen <mattmaslen123@gmail.com>
2020-11-05 14:14:52 +0000

    added type safety

--- 05-autoloading/app/Hello.php
+++ 05-autoloading/app/Hello.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App;
 
 class Hello
 {
-    public function hello($name)
+    public function hello(string $name) : string 
     {
         return "Hello {$name}";
     }
--- 05-autoloading/app/Stuff/Things/Potato.php
+++ 05-autoloading/app/Stuff/Things/Potato.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /* 
 Create a class Potato in the App\Stuff\Things namespace. It should have a water() and hasGrown() method. hasGrown() should return false until the Potato has been watered five or more times. Put the following in index.php and then check that you get back the right results:
  */
@@ -10,12 +12,12 @@ class Potato
 {
     private $watered = 0;
 
-    public function water()
+    public function water() : void
     {
-        $this->watered += 1; 
+        $this->watered += 1;
     }
 
-    public function hasGrown()
+    public function hasGrown() : bool
     {
         return $this->watered >= 5;
     }
